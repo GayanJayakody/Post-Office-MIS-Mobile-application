583c5b9a441341de48f3aec1059c431129be3407
Gayan Jayakody <gayanjayakody13@gmail.com>
2020-08-20 22:30:09 +0530

    delpoy to heroku

---
+++ App.js
@@ -0,0 +1,73 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, AppRegistry, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Delivered from './screens/MarkAsDelivered';
+import Scan from './screens/QR';
+import Map from './screens/GoogleMap';
+
+const windowWidth = Dimensions.get('window').width;
+
+function HomeScreen({ navigation }) {
+  return (
+	<ImageBackground source={require('./images/wallpaper.jpg')} style={{flex:1}}>
+    <View style={Styles.container}>
+	  	<View style={Styles.top}/>
+	  	<View style={Styles.bottom}>
+	  	<TouchableOpacity 
+			activeOpacity={0.5} 
+			onPress = {() => navigation.openDrawer()}>
+    		<Image source={require('./images/envilope.png')} style={Styles.image}/>
+	  	</TouchableOpacity>
+		</View>
+    </View>
+	</ImageBackground>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+		drawerContentOptions={{
+    	  	activeTintColor: 'black',
+			inactiveTintColor: 'goldenrod',
+			activeBackgroundColor: 'white',
+    	  	itemStyle: { marginTop:30},
+			labelStyle: {fontWeight: '900',fontSize: 20}
+        }}
+		drawerStyle={{
+          	backgroundColor: 'lavenderblush',
+   		  	width: 200,
+  		}}
+	  initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+		<Drawer.Screen name="Deliver" component={Delivered} />
+		<Drawer.Screen name="Scan QR" component={Scan} />
+		<Drawer.Screen name="Map" component={Map} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+	
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+	alignItems: 'center',
+  },
+  image: {
+    height: windowWidth*341/433/2,
+    width: windowWidth/2,
+    resizeMode: 'center',
+  },
+  top: {
+    flex: 0.525,
+  },
+  bottom: {
+    flex: 0.3,
+  },
+});
---
+++ screens/QR.js
@@ -0,0 +1,116 @@
+
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { Text, View, TouchableHighlight, PermissionsAndroid, Platform, ImageBackground, Image, Dimensions } from 'react-native';
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
+import {Button} from 'react-native-elements';
+import { QRStyles } from './styles';
+
+
+export default class Scan extends Component {
+  constructor() {
+    super();
+    this.state = {
+      qrvalue: '',
+      opneScanner: false,
+	  getqrvalues: false
+    };
+  }
+  
+  onBarcodeScan(qrvalue) {
+    //called after te successful scanning of QRCode/Barcode
+    this.setState({ opneScanner: true });
+	this.setState({ getqrvalues: true });
+	let qr_content = qrvalue.split('_');
+	if(qr_content === undefined || qr_content[0] === undefined ||qr_content[1] === undefined){
+        alert('Invalid QR Code');
+          }
+	else{
+        let content1 = qr_content[0].split('=');
+        let content2 = qr_content[1].split('=');
+		let content3 = qr_content[2].split('=');
+		if (content1[1]==='RegPost'){
+			var content4 = qr_content[3].split('=');
+		}
+        if(content1[0] === 'type' && content2[0] === 'id' && content1[1]==='RegPost'){
+			this.setState({ qrvalue: 'Type = '+content1[1]+'\nId      = '+content2[1]+'\n\nDelivery Address = '+content3[1]+'\n\nSender Address = '+content4[1] });
+        }
+        else if(content1[0] === 'type' && content2[0] === 'id'){
+			this.setState({ qrvalue: 'Type = '+content1[1]+'\nId      = '+content2[1]+'\n\nDelivery Address = '+content3[1] });
+        }
+        else{
+            alert('Invalid QR Code');
+        }                
+      }
+  }
+  
+  // check camera permission
+  onOpneScanner() {
+    var that =this;
+    if(Platform.OS === 'android'){
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,{
+              'title': 'Camera Permission',
+              'message': 'Post Office MIS needs access to your camera ',
+			  buttonPositive: "OK"
+            }
+          )
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If CAMERA Permission is granted
+            that.setState({ qrvalue: ''});
+            that.setState({ opneScanner: true });
+          } else {
+            alert("CAMERA permission denied");
+          }
+        } catch (err) {
+          alert("Camera permission err",err);
+          console.warn(err);
+        }
+      }
+      //Calling the camera permission function
+      requestCameraPermission();
+    }else{
+      that.setState({ qrvalue: '' });
+      that.setState({ opneScanner: true });
+    }    
+  }
+  
+  render() {
+    if (!this.state.opneScanner) {
+      	return (
+			<ImageBackground source={require('../images/wallpaper.jpg')} style={{flex:1}}>
+        		<View style={QRStyles.container}>
+					{this.onOpneScanner()}
+        		</View>
+			</ImageBackground>
+      	);
+    }
+	else{
+	    return (
+	      <View style={{flex:1}}>
+		  	<View style={{flex:0.6}}>
+			<Text style={QRStyles.simpleText}>Scan the QR code{'\n'}</Text>
+	        	<CameraKitCameraScreen
+	          		showFrame={false}
+	          		scanBarcode={true}
+	          		laserColor={'blue'}
+	          		frameColor={'yellow'}
+	          		colorForScannerFrame={'black'}
+	          		onReadCode={event => this.onBarcodeScan(event.nativeEvent.codeStringValue)}
+	        	/>
+			</View>
+			<View style={{flex:0.4}}>
+			<Image source={require('../images/background.jpg')} style={QRStyles.image}/>
+				<View style={QRStyles.subContainer}>
+			  	<Text style={QRStyles.simpleText}>{this.state.qrvalue ? this.state.qrvalue : ''}{'\n'}</Text>
+
+			  </View>
+			</View>
+	      </View>
+		
+	    );
+	}
+  }
+}
