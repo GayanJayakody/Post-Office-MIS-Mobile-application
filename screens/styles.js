67820056e3a9fedbc0b54e454f50e0cdcd394a32
Gayan Jayakody <gayanjayakody13@gmail.com>
2020-09-03 19:14:57 +0530

    Update styles.js

--- screens/styles.js
+++ screens/styles.js
@@ -1,4 +1,4 @@
-import { Dimensions,StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 
 const windowWidth = Dimensions.get('window').width;
@@ -6,96 +6,96 @@ const windowWidth = Dimensions.get('window').width;
 
 const MarkAsDeliveredStyles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   subContainer: {
-	position: 'absolute',
-	left: '25%',
+    position: 'absolute',
+    left: '25%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   simpleText: {
-	textAlign: 'center',
+    textAlign: 'center',
     color: 'goldenrod',
-	fontWeight: 'bold',
-    fontSize: 25, 
-    alignSelf: 'center', 
-    padding: 10, 
+    fontWeight: 'bold',
+    fontSize: 25,
+    alignSelf: 'center',
+    padding: 10,
   },
   image: {
     height: '100%',
     width: windowWidth,
   },
-  buttonDeliver:{
-	  backgroundColor: 'goldenrod',
-	  borderRadius:10,
-	  width: 200,
-	  height:100 	
+  buttonDeliver: {
+    backgroundColor: 'goldenrod',
+    borderRadius: 10,
+    width: 200,
+    height: 100
   },
-  buttonUnavailable:{
-	  backgroundColor: 'goldenrod',
-	  borderRadius:10,
-	  width: 200,
-	  height:40 	
+  buttonUnavailable: {
+    backgroundColor: 'goldenrod',
+    borderRadius: 10,
+    width: 200,
+    height: 40
   },
-  buttonTitle:{
-	  color: "white",
-	  fontSize: 20
+  buttonTitle: {
+    color: "white",
+    fontSize: 20
   }
 });
 
 const QRStyles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   subContainer: {
-	position: 'absolute',
+    position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  simpleText: { 
+  simpleText: {
     color: 'goldenrod',
-	fontWeight: '900',
-    fontSize: 20, 
+    fontWeight: '900',
+    fontSize: 20,
     alignSelf: 'center',
-	padding: 10
+    padding: 10
 
   },
-  absoluteText: { 
-	position: 'absolute',
+  absoluteText: {
+    position: 'absolute',
     color: 'goldenrod',
-	fontWeight: '900',
-    fontSize: 25, 
-    alignSelf: 'center', 
-    padding: 10, 
+    fontWeight: '900',
+    fontSize: 25,
+    alignSelf: 'center',
+    padding: 10,
   },
   image: {
     height: '100%',
     width: windowWidth,
   },
-  button:{
-	  backgroundColor: 'goldenrod',
-	  borderRadius:10,
-	  width: 200,
-	  height:40,
+  button: {
+    backgroundColor: 'goldenrod',
+    borderRadius: 10,
+    width: 200,
+    height: 40,
   },
-  buttonTitle:{
-	  color: "white",
-	  fontSize: 20
+  buttonTitle: {
+    color: "white",
+    fontSize: 20
   }
 });
 
 const MapStyles = StyleSheet.create({
-	container:{
-		...StyleSheet.absoluteFillObject
-	},
-	map:{
-		...StyleSheet.absoluteFillObject
-	}
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
 })
-	
-export { MapStyles,QRStyles, MarkAsDeliveredStyles} 
+
+export { MapStyles, QRStyles, MarkAsDeliveredStyles } 
