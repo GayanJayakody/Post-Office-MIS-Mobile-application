ea55cda842e4127ceb0e268ba87b3fedd8783a07
Gayan Jayakody <gayanjayakody13@gmail.com>
2020-09-03 19:15:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { StyleSheet, View, Image, Text, AppRegistry, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import Delivered from './screens/MarkAsDelivered';
+import PropTypes from 'prop-types';
+import Delivered from './screens/MarkAsDelivered'
 import Scan from './screens/QR';
 import Map from './screens/GoogleMap';
-
+	
 const windowWidth = Dimensions.get('window').width;
 
 function HomeScreen({ navigation }) {
   return (
 	<ImageBackground source={require('./images/wallpaper.jpg')} style={{flex:1}}>
     <View style={Styles.container}>
-	  	<View style={Styles.top}/>
-	  	<View style={Styles.bottom}>
-	  	<TouchableOpacity 
-			activeOpacity={0.5} 
-			onPress = {() => navigation.openDrawer()}>
-    		<Image source={require('./images/envilope.png')} style={Styles.image}/>
-	  	</TouchableOpacity>
-		</View>
+	<View style={Styles.top}/>
+	<View style={Styles.bottom}>
+	<TouchableOpacity 
+		activeOpacity={0.5} 
+		onPress = {() => navigation.openDrawer()}>
+    <Image source={require('./images/envilope.png')} style={Styles.image}/>
+	</TouchableOpacity>
+	</View>
     </View>
 	</ImageBackground>
   );
@@ -28,23 +29,20 @@ function HomeScreen({ navigation }) {
 
 const Drawer = createDrawerNavigator();
 
+HomeScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator
-		drawerContentOptions={{
-    	  	activeTintColor: 'black',
-			inactiveTintColor: 'goldenrod',
-			activeBackgroundColor: 'white',
-    	  	itemStyle: { marginTop:30},
-			labelStyle: {fontWeight: '900',fontSize: 20}
-        }}
-		drawerStyle={{
-          	backgroundColor: 'lavenderblush',
-   		  	width: 200,
-  		}}
-	  initialRouteName="Home">
+		initialRouteName = "Home"
+		drawerContentOptions={{activeTintColor:'black',
+		inactiveTintColor:'goldenrod',
+		activeBackgroundColor:'white',itemStyle : {marginTop:30},
+		labelStyle : {fontWeight: '900',fontSize: 20}}}
+		drawerStyle={{backgroundColor: 'lavenderblush', width: 200}}>
         <Drawer.Screen name="Home" component={HomeScreen} />
 		<Drawer.Screen name="Deliver" component={Delivered} />
 		<Drawer.Screen name="Scan QR" component={Scan} />
