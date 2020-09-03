d722c80cda5a9a3d25375d7f0c264cf506450680
Gayan Jayakody <gayanjayakody13@gmail.com>
2020-09-03 19:16:46 +0530

    Create GoogleMap-test.js

---
+++ __tests__/GoogleMap-test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from '../screens/GoogleMap';
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Map />).toJSON();
+  expect(tree).toMatchSnapshot();
+  jest.useFakeTimers(); 
+});
